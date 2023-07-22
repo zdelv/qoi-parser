a8bafdc67cb8b1bfb22c102d9d6916f96deb20a9
Zach DelVecchio <zachdelvecchio@gmail.com>
2023-07-22 00:00:20 -0400

    Add streaming decoder
    Big bulk commit that adds StreamDecoder, which is a byte-by-byte
    streaming decoder for the QOI standard. This allows for a user to parse
    a QOI image without having to load the entire image into memory. Each
    byte can be passed into the parser and individual resulting pixels can
    be handled as they appear.
    
    This is implemented as a big state machine operating on a struct. Users
    feed bytes in using the StreamDecoder::feed() method, and results of
    type StreamDecoderOutput are returned. These results inform the user of
    whether there are pixels available, more data is need, or if the image
    is finished. All outputs are wrapped in a Result, allowing for error
    propagation while parsing.
    
    This implementation is verbose, probably slow, and needs some cleanup.
    I'd like to go through and abstract out to methods to cleanup the state
    machine. This also probably uses more memory than it needs to. Also, the
    dec.rs file is nearly 1000 lines long. It need to be broken out into
    more files.

--- src/bin/qoi-parser.rs
+++ src/bin/qoi-parser.rs
@@ -22,6 +22,78 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 mod tests {
+    #[test]
+    fn test_save_stream_decoder() {
+        use image::codecs::png::PngEncoder;
+        use image::ImageEncoder;
+        use qoiparser::{Pixel, StreamDecoder, StreamDecoderOutput};
+        use std::fs::File;
+        use std::io::{BufReader, Read};
+
+        let file = BufReader::new(File::open("tests/dice.qoi").unwrap());
+        let mut iter = file.bytes();
+
+        let mut sdec = StreamDecoder::new();
+
+        let mut width: u32 = 0;
+        let mut height: u32 = 0;
+
+        let mut img_size: u64 = 0;
+        let mut img: Vec<Pixel> = Vec::new();
+
+        while let Some(b) = iter.next() {
+            match b {
+                Ok(byte) => {
+                    match sdec.feed(byte).unwrap() {
+                        // The StreamDecoder informs us if it needs more bytes after recieving one
+                        // byte. This allows us to work on just getting those bytes and checking
+                        // the state again later.
+                        StreamDecoderOutput::NeedMore(_) => {}
+
+                        // After recieving the image size, we can reserve space for the image
+                        // buffer.
+                        StreamDecoderOutput::ImageWidthParsed(w) => {
+                            width = w;
+                            img_size = w as u64;
+                        }
+                        StreamDecoderOutput::ImageHeightParsed(h) => {
+                            height = h;
+                            img_size *= h as u64;
+                            img.reserve_exact(img_size as usize);
+                        }
+
+                        // When pixels are ready to be produced, the StreamDecoder returns an
+                        // iterator that produces those pixels. This is a lightweight iterator,
+                        // with just a Pixel and u8 count attached (5 bytes in total).
+                        StreamDecoderOutput::Pixels(it) => {
+                            for pix in it {
+                                img.push(pix);
+                            }
+                        }
+
+                        // The StreamDecoder informs us when it has returned all pixels in the
+                        // image.
+                        StreamDecoderOutput::Finished => break,
+                        _ => {}
+                    }
+                }
+                // If we failed to pull a byte out of the file, then throw an error.
+                Err(e) => {
+                    println!("{}", e);
+                    assert!(false)
+                }
+            }
+        }
+
+        let png_enc = PngEncoder::new(File::create("tests/output_stream.png").unwrap());
+
+        let buf: Vec<u8> = img.into_iter().flat_map(|a| a.to_bytes()).collect();
+
+        png_enc
+            .write_image(&buf, width, height, image::ColorType::Rgba8)
+            .unwrap();
+    }
+
     /// Not really a test, but more of a "input" == "output" where the two must be manually
     /// checked.
     #[test]
